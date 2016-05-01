6fb432145b793183da7172ad5bc57c8df265892b
Jaya Wijono <jayzzwijono@yahoo.com>
2016-05-02 01:19:35 +1000

    refactor exceoption handling

--- jspec.rb
+++ jspec.rb
@@ -12,6 +12,26 @@ class Test
     puts
   end
 
+  def self.run
+    public_instance_methods.grep(/_test$/).each do |name|
+      e = self.new.run name
+      unless e then
+        print '.'
+      else 
+        puts
+        puts "Failure: #{self}##{name}: #{e.message}"
+        puts " #{e.backtrace.first}"
+      end
+    end
+  end
+
+  def run name
+    send name
+    false
+  rescue => e
+    e
+  end
+
   def assert test, msg= "Failed test"
     unless test then
       bt = caller.drop_while { |s| s =~ /#{__FILE__}/ }
--- test_example_3.rb
+++ test_example_3.rb
@@ -1,22 +1,6 @@
 require './jspec'
 
 class ZTest < Test
-  def self.run
-    public_instance_methods.grep(/_test$/).each do |name|
-      begin
-        self.new.run name
-        print '.'
-      rescue => e
-        puts "Failure: #{self}##{name}: #{e.message}"
-        puts " #{e.backtrace.first}"
-      end
-    end
-  end
-
-  def run name
-    send name
-  end
-  
   def first_test
     a = 1
     assert_equal 1, a
